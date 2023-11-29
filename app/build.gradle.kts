1e24aa00f6acace4c4799868f5007231e34b310e
ghadizana <ghadizanauraaliya@gmail.com>
2023-11-29 08:08:20 +0700

    adding splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.timtiga.recipe_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.timtiga.recipe_app"
@@ -42,7 +42,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
