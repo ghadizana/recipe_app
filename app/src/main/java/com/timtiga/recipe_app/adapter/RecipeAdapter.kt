69090380f0d1a31d820548bf22691f874da4dc3a
ghadizana <ghadizanauraaliya@gmail.com>
2023-11-29 13:44:03 +0700

    add the onBindViewHolder

--- app/src/main/java/com/timtiga/recipe_app/adapter/RecipeAdapter.kt
+++ app/src/main/java/com/timtiga/recipe_app/adapter/RecipeAdapter.kt
@@ -1,8 +1,12 @@
 package com.timtiga.recipe_app.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.timtiga.recipe_app.DetailActivity
+import com.timtiga.recipe_app.R
 import com.timtiga.recipe_app.databinding.ItemRecipeBinding
 import com.timtiga.recipe_app.model.network.Meals
 
@@ -23,6 +27,23 @@ class RecipeAdapter: RecyclerView.Adapter<RecipeAdapter.MyViewHolder>() {
     override fun getItemCount(): Int = listRecipe.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = listRecipe[position]
 
+        holder.binding.apply {
+            tvTitleRecipe.text = data.strMeal
+            tvCategory.text = data.strCategory
+            Picasso
+                .get()
+                .load(data.strMealThumb)
+                .resize(2048, 1600)
+                .placeholder(R.drawable.ic_splash)
+                .into(ivRecipe)
+        }
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(it.context, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.LIST_RECIPE, data)
+            it.context.startActivity(intent)
+        }
     }
 }
