cae4d6e0bc115c877754a23d6e1de900811eab65
bebyazzura <bebyazzura6@gmail.com>
2023-11-29 11:38:09 +0700

    enable binding method

--- app/src/main/java/com/timtiga/recipe_app/MainActivity.kt
+++ app/src/main/java/com/timtiga/recipe_app/MainActivity.kt
@@ -3,11 +3,16 @@ package com.timtiga.recipe_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.timtiga.recipe_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        _binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
