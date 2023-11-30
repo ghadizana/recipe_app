d33d982a3a223e14ee3f7f7fcd2ff1dc1e36b08b
ghadizana <ghadizanauraaliya@gmail.com>
2023-11-30 14:01:55 +0700

    main activity

--- app/src/main/java/com/timtiga/recipe_app/adapter/RecipeAdapter.kt
+++ app/src/main/java/com/timtiga/recipe_app/adapter/RecipeAdapter.kt
@@ -10,32 +10,32 @@ import com.timtiga.recipe_app.model.network.RecipeResponse
 
 class RecipeAdapter(private val data: MutableList<RecipeResponse.meals> = mutableListOf()) : RecyclerView.Adapter<RecipeAdapter.MyViewHolder>() {
     private val listRecipe = ArrayList<RecipeResponse.meals>()
-    class MyViewHolder(var binding: ItemRecipeBinding):
-            RecyclerView.ViewHolder(binding.root)
-
-    fun setData(list: RecipeResponse.meals) {
-        this.data.clear()
-        this.data.addAll(listOf(list))
+    class MyViewHolder(var binding: ItemRecipeBinding): RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item: RecipeResponse.meals) {
+            Picasso
+                .get()
+                .load(item.strMealThumb)
+                .resize(2048, 1600)
+                .placeholder(R.drawable.ic_splash)
+                .into(binding.ivRecipe)
+            binding.tvRecipe.text = item.strMeal
+            binding.tvCategory.text = item.strCategory
+        }
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =  MyViewHolder (
-        ItemRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-    )
+
+    fun setData(list: MutableList<RecipeResponse.meals>) {
+        this.data.clear()
+        this.data.addAll(list)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(ItemRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun getItemCount(): Int = listRecipe.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val data = listRecipe[position]
 
-        holder.binding.apply {
-            tvRecipe.text = data.strMeal
-            tvCategory.text = data.strCategory
-            Picasso
-                .get()
-                .load(data.strMealThumb)
-                .resize(2048, 1600)
-                .placeholder(R.drawable.ic_splash)
-                .into(ivRecipe)
-        }
+        holder.onBind(data)
 
 //        holder.itemView.setOnClickListener {
 //            val intent = Intent(it.context, DetailActivity::class.java)
--- app/src/main/java/com/timtiga/recipe_app/ui/MainActivity.kt
+++ app/src/main/java/com/timtiga/recipe_app/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.timtiga.recipe_app.ui
 
+import android.app.Notification.Action
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.timtiga.recipe_app.RecipeViewModel
+import com.timtiga.recipe_app.Result
 import com.timtiga.recipe_app.adapter.RecipeAdapter
 import com.timtiga.recipe_app.databinding.ActivityMainBinding
 import com.timtiga.recipe_app.model.network.RecipeResponse
@@ -23,15 +26,31 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         _binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val mAdapter = RecipeAdapter()
+
+        binding.rvRecipes.setHasFixedSize(true)
+        binding.rvRecipes.adapter = mAdapter
         showRecyclerList()
 
-        _recipeViewModel = ViewModelProvider(this)[RecipeViewModel::class.java]
-        recipeViewModel.listRecipes()
-        recipeViewModel.listRecipes.observe(this){
-            val mAdapter = RecipeAdapter()
-            binding.rvRecipes.apply {
-                adapter = mAdapter
-        }
+//        _recipeViewModel = ViewModelProvider(this)[RecipeViewModel::class.java]
+//        recipeViewModel.listRecipes()
+//        recipeViewModel.listRecipes.observe(this){
+//            binding.rvRecipes.apply {
+//                adapter = mAdapter
+//        }
+
+        recipeViewModel.listRecipes.observe(this) {
+            when (it) {
+                is Result.Success<*> -> {
+                    mAdapter.setData(it.data as MutableList<RecipeResponse.meals>)
+                }
+                is Result.Error -> {
+                    Toast.makeText(this, it.exception.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is Result.Loading -> {
+                    binding.
+                }
+            }
     }
     }
 
