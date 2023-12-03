71c1080d6e56ee418de9dbab8ccc3b84e599c085
ghadizana <ghadizanauraaliya@gmail.com>
2023-12-03 15:48:22 +0700

    detail activity

--- app/src/main/java/com/timtiga/recipe_app/model/network/RecipeResponse.kt
+++ app/src/main/java/com/timtiga/recipe_app/model/network/RecipeResponse.kt
@@ -38,31 +38,3 @@ data class MealsItem (
     @Json(name = "strMeasure1")
     val strMeasure1: String
 ): Parcelable
-
-//@JsonClass(generateAdapter = true)
-//@Parcelize
-//data class Ingredient (
-//    val strIngredient1: String,
-//    val strIngredient2: String,
-//    val strIngredient3: String,
-//    val strIngredient4: String,
-//    val strIngredient5: String,
-//    val strIngredient6: String,
-//    val strIngredient7: String,
-//    val strIngredient8: String,
-//    val strIngredient9: String,
-//): Parcelable
-//
-//@JsonClass(generateAdapter = true)
-//@Parcelize
-//data class Measure (
-//    val strMeasure1: String,
-//    val strMeasure2: String,
-//    val strMeasure3: String,
-//    val strMeasure4: String,
-//    val strMeasure5: String,
-//    val strMeasure6: String,
-//    val strMeasure7: String,
-//    val strMeasure8: String,
-//    val strMeasure9: String,
-//): Parcelable
